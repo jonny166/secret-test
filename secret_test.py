c3cc8e9b0d15b58251c0d28c135c2a5cea07179d
jonny166 <kathy.church@gmail.com>
2015-05-28 23:37:49 -0400

    Polish secret_test code
    Incorporate changes from pylint

--- secret_test.py
+++ secret_test.py
@@ -25,7 +25,7 @@ def _get_primes(n):
       Taken from the Python Cookbook
       http://archive.oreilly.com/pub/a/python/excerpt/pythonckbk_chap1/index1.html?page=last
     """
-    D = {  }
+    D = {}
     yield 2
     for q in itertools.islice(itertools.count(3), 0, n-2, 2):
         p = D.pop(q, None)
@@ -34,30 +34,22 @@ def _get_primes(n):
             yield q
         else:
             x = p + q
-            while x in D or not (x&1):
+            while x in D or not x&1:
                 x += p
             D[x] = p
 #end _get_primes
 
 
-def secret1(n):
-    return n
-
-def secret2(n):
-    if n < 30:
-        return n
-    else:
-        return -1
-
 def validate_secret(secret, n):
-    """Validate secret(x) + secret(y) == secret(x + y) for all combinations primes < n
+    """Validate secret(x) + secret(y) == secret(x + y) for all primes < n
 
     Args:
       secret (func): Function to validate. Should expect and return an int.
       n (int): Yield all primes less than n
 
     Returns:
-      Bool: False if secret(x) + secret(y) != secret(x+y) for some primes x and y, otherwise True
+      Bool: False if secret(x) + secret(y) != secret(x+y) for some primes \
+x and y, otherwise True
 
     Raises:
       ValueError: If secret is not a function that takes and returns an int
@@ -66,19 +58,20 @@ def validate_secret(secret, n):
 
     if not isinstance(n, (int, long)):
         raise TypeError("n must be an int")
-        
+
     primes = []
     for prime in _get_primes(n):
         try:
-            if not all(secret(p + prime) == secret(p) + secret(prime) for p in primes):
+            if not all(secret(p + prime) ==
+                       secret(p) + secret(prime) for p in primes):
                 logger.info("secret({0}) + secret(n) != secret({0}+n) for all primes < {0}".format(prime))
                 return False
             primes.append(prime)
-            
-        except ValueError, e:
+
+        except ValueError:
             logger.error(traceback.format_exc())
             raise ValueError("secret must accept and return an integer")
-        except TypeError, e:
+        except TypeError:
             logger.error(traceback.format_exc())
             raise ValueError("secret must accept and return an integer")
 
