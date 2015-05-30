2005e86306ae3bf40ecb4c4296d1965f0fc9e5c9
jonny166 <kathy.church@gmail.com>
2015-05-29 20:03:04 -0400

    Create README and move write log messages to file
    Created a README with usage example.
    Logged start of each unit test.
    Adding a FileHandler for the log messages to the unit test file so
    they stop going to stdout.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # secret-test
+
 For a secret function, verify that secret(x) + secret(y) == secret(x+y) for all combinations of primes &lt; n
+
+## Usage
+```python
+# test.py
+import secret_test
+
+def good_secret(n): 
+    return n
+
+def bad_secret(n): 
+    return n-1
+
+print secret_test.validate_secret(good_secret, 99)
+print secret_test.validate_secret(bad_secret, 99)
+```
+$ python2.7 test.py
+True
+False
+
+## Installation
+
+Just download secret_test.py.  
+The only dependency is python 2.7.
+
+## API Reference
+
+Depending on the size of the project, if it is small and simple enough the reference docs can be added to the README. For medium size to larger projects it is important to at least provide a link to where the API reference docs live.
+
+## Tests
+
+You can run the provided test file to verify it is working properly.
+
+$ ./secret_test_unit_test.py 
+.......
+----------------------------------------------------------------------
+Ran 7 tests in 0.021s
+
+OK
+
--- secret_test_unit_test.py
+++ secret_test_unit_test.py
@@ -6,10 +6,14 @@
 import unittest
 import logging
 import secret_test
+import inspect
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
-secret_test.logger.setLevel(logging.DEBUG)
-secret_test.logger.addHandler(logging.StreamHandler())
+
+logger = logging.getLogger(secret_test.__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.FileHandler(secret_test.__name__ + ".log", "w"))
+logger.propagate = False
 
 ###SOME SECRETS TO TEST###
 def secret1(n):
@@ -36,33 +40,40 @@ class SecretValidatorTests(unittest.TestCase):
 
     def test_success(self):
         """The secret function should pass"""
+        logger.debug("###Starting Test: " + inspect.getframeinfo(inspect.currentframe())[2])
         self.assertTrue(secret_test.validate_secret(secret1, 101))
 
     def test_bad_secret(self):
         """The secret function should fail"""
+        logger.debug("###Starting Test: " + inspect.getframeinfo(inspect.currentframe())[2])
         self.assertFalse(secret_test.validate_secret(secret2, 19))
 
     def test_boundary_bad_secret(self):
         """Bad function, but n not large enough to trigger failure"""
+        logger.debug("###Starting Test: " + inspect.getframeinfo(inspect.currentframe())[2])
         self.assertTrue(secret_test.validate_secret(secret2, 18))
                         
     def test_secret_bad_return(self):
         """Secret does not return an int"""
+        logger.debug("###Starting Test: " + inspect.getframeinfo(inspect.currentframe())[2])
         with self.assertRaises(ValueError):
             secret_test.validate_secret(secret3, 100)
                         
     def test_secret_bad_args(self):
         """Secret does not expect an int"""
+        logger.debug("###Starting Test: " + inspect.getframeinfo(inspect.currentframe())[2])
         with self.assertRaises(ValueError):
             secret_test.validate_secret(secret3, 100)
 
     def test_pass_bad_n_arg(self):
         """Pass validate_secrate invalid n"""
+        logger.debug("###Starting Test: " + inspect.getframeinfo(inspect.currentframe())[2])
         with self.assertRaises(TypeError):
             secret_test.validate_secret(secret1, "not a number")
 
     def test_pass_bad_secret_arg(self):
         """Pass validate_secrate invalid secret"""
+        logger.debug("###Starting Test: " + inspect.getframeinfo(inspect.currentframe())[2])
         with self.assertRaises(ValueError):
             secret_test.validate_secret(55, 300)
         
